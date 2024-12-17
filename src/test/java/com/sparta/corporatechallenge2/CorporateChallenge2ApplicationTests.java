561b8d6d707abad750744b7bfc206deae703a721
hsilnam <hsilnam3@gmail.com>
2024-12-17 13:47:57 +0900

    init: 프로젝트 init

---
+++ src/test/java/com/sparta/corporatechallenge2/CorporateChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.corporatechallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateChallenge2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
